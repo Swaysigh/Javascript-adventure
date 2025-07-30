5e765a3193368c1bee82c084a46cd1d735b9a9e7
Swayam <assentioncreed3@gmail.com>
2025-07-30 14:56:07 +0530

    added code

---
+++ Basics.js
@@ -0,0 +1,37 @@
+
+// Write code below 💖
+
+console.log("Hello internet!");
+console.log("Stay Frosty! Soldier.");
+
+console.log("                 KFC ")
+console.log("             Order Details ")
+console.log(" Items           Quantity            Price")
+console.log(" Burger             1                 250")
+console.log(" French Fries       1                 200")
+console.log(" Soda               1                 100")
+console.log(" Total                                550")
+
+// Let and Const
+const FirstName = "Swayam Singh"
+let CurrentLocation = "Bhilai"
+const FavColor = "Beige"
+let Mood = "Happy"
+
+console.log(FirstName,"\n" ,CurrentLocation,"\n" ,FavColor,'\n' ,Mood)
+
+CurrentLocation = "Chhattisgarh"      // Reassigning the value of CurrentLocation
+console.log(CurrentLocation)
+
+// Variables and Data Types
+const comapnyName = "Reliance"     //string
+const foundingYear = 2005         //number
+let isActive = true              //boolean
+let fundingAmount               //undefined
+console.log(comapnyName, "\n", foundingYear, "\n", isActive, "\n" ,fundingAmount)
+
+// Converting Fahrenheit to Celsius
+// Formula: (F - 32) / 1.8 = C
+let fahrenheit = 82
+let celcius = (fahrenheit - 32)/1.8
+console.log(celcius)
